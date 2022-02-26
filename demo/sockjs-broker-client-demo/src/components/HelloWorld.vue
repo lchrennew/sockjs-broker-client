23e81aff72c0e11f8ccf4b553dffbdee7e4e8ec9
lichun <lchrennew@126.com>
2022-02-26 22:41:57 +0800

    v1.0.11

---
+++ demo/sockjs-broker-client-demo/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Client from "sockjs-broker-client";
+
+const client = new Client('http://localhost:9999/queues');
+const sub = () => client.subscribe('topic1', ({ data }) => console.log(data)),
+    unsub = () => client.unsubscribe('topic1'),
+    pub = () => client.send('topic1', 'hello,world'),
+    conn = () => client.connect(),
+    disconn = () => client.disconnect()
+</script>
+
+<template>
+    <button @click="conn">接入</button>
+    <button @click="sub">订阅</button>
+    <button @click="unsub">取消订阅</button>
+    <button @click="pub">推送</button>
+    <button @click="disconn">断开</button>
+</template>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
